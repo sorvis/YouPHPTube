9f5ca210eb02d1cb4ad3a49ec866ce9e8e1656d8
Steven Orvis <steven.orvis@geneva.edu>
2018-01-26 20:27:25 -0500

    Added in docker files from leejoneshane/docker-youphptube

---
+++ configuration.php
@@ -0,0 +1,14 @@
+<?php
+$global['webSiteRootURL'] = 'PROTOCOL://DOMAIN/';
+$global['systemRootPath'] = '/var/www/localhost/htdocs/';
+
+$mysqlHost = 'DB_HOST';
+$mysqlUser = 'DB_USER';
+$mysqlPass = 'DB_PASSWORD';
+$mysqlDatabase = 'youPHPTube';
+
+/**
+ * Do NOT change from here
+ */
+
+require_once $global['systemRootPath'].'objects/include_config.php';
---
+++ docker-entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+set -euo pipefail
+
+if [[ "${DOMAIN}" != "your.domain" && "${DB_HOST}" != "localhost" ]]; then
+  if [ ! -f videos/configuration.php ]; then
+    cp /home/configuration.php /var/www/localhost/htdocs/videos/configuration.php
+    sed -ri \
+        -e "s!PROTOCOL!${DOMAIN_PROTOCOL}!g" \
+        -e "s!DOMAIN!${DOMAIN}!g" \
+        -e "s!DB_HOST!${DB_HOST}!g" \
+        -e "s!DB_USER!${DB_USER}!g" \
+        -e "s!DB_PASSWORD!${DB_PASSWORD}!g" \
+        "/var/www/localhost/htdocs/videos/configuration.php"
+  fi
+  
+  if mysqlshow --host=${DB_HOST} --user=${DB_USER} --password=${DB_PASSWORD} youPHPTube; then
+    echo "database exist!"
+  else
+    echo "CREATE DATABASE youPHPTube CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;" | mysql --host="${DB_HOST}" --user="${DB_USER}" --password="${DB_PASSWORD}"
+    mysql --host="${DB_HOST}" --user="${DB_USER}" --password="${DB_PASSWORD}" youPHPTube < /var/www/localhost/htdocs/install/database.sql
+    echo "USE youPHPTube; INSERT INTO users (id, user, password, created, modified, isAdmin) VALUES (1, 'admin', md5('${ADMIN_PASSWORD}'), now(), now(), true);" | mysql --host="${DB_HOST}" --user="${DB_USER}" --password="${DB_PASSWORD}"
+    echo "USE youPHPTube; INSERT INTO categories (id, name, clean_name, created, modified) VALUES (1, 'Default', 'default', now(), now());" | mysql --host="${DB_HOST}" --user="${DB_USER}" --password="${DB_PASSWORD}"
+    echo "USE youPHPTube; INSERT INTO configurations (id, video_resolution, users_id, version, webSiteTitle, language, contactEmail,  created, modified, encoderURL) VALUES (1, '858:480', 1,'4.0', '${SITE_TITLE}', '${LANG}', '${ADMIN_EMAIL}', now(), now(), 'https://${DOMAIN}/encoder');" | mysql --host="${DB_HOST}" --user="${DB_USER}" --password="${DB_PASSWORD}"
+  fi
+
+  if mysqlshow --host=${DB_HOST} --user=${DB_USER} --password=${DB_PASSWORD} youPHPTube-Encoder; then
+    echo "database exist!"
+  else
+    mysql --host="${DB_HOST}" --user="${DB_USER}" --password="${DB_PASSWORD}" -e "CREATE DATABASE \`youPHPTube-Encoder\` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+    mysql --host="${DB_HOST}" --user="${DB_USER}" --password="${DB_PASSWORD}" youPHPTube-Encoder < /var/www/localhost/htdocs/encoder/install/database.sql
+    mysql --host="${DB_HOST}" --user="${DB_USER}" --password="${DB_PASSWORD}" -e "USE \`youPHPTube-Encoder\`; INSERT INTO streamers (id, siteURL, user, pass, priority, isAdmin, created, modified) VALUES (1, 'https://${DOMAIN}', 'admin', '${ADMIN_PASSWORD}', 1, 1, now(), now());"
+    mysql --host="${DB_HOST}" --user="${DB_USER}" --password="${DB_PASSWORD}" -e "USE \`youPHPTube-Encoder\`; INSERT INTO configurations (id, allowedStreamersURL, defaultPriority, created, modified, version) VALUES (1, 'https://${DOMAIN}', 1, now(), now(), '1.0');"
+  fi
+fi
+
+rm -f /run/apache2/httpd.pid
+exec httpd -DFOREGROUND
